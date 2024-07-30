55ee4933a96c909eabe1172901b9a6b2ad76cbb0
explorer14 <amanag1983@gmail.com>
2024-07-30 14:40:16 +0200

    Expose types within the module

---
+++ WeatherReporting/WeatherReporting.DomainModel/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("WeatherReporting.ExternalSources.Adapter")]
+[assembly:InternalsVisibleTo("WeatherReporting.Publishing.Adapter")]
+[assembly:InternalsVisibleTo("WeatherReporting.ApplicationServices")]
