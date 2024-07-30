b6826f20f84e8f5169a535a5a2e643193c964569
Aman Agrawal <aman.agrawal@coolblue.nl>
2024-07-30 14:39:47 +0200

    Implement the new published interface in the adapter (at the edge)

---
+++ WeatherReporting/WeatherReporting.Publishing.Adapter/InmemoryQueuePublisher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using WeatherReporting.PublishedInterfaces;
+
+namespace WeatherReporting.Publishing.Adapter;
+
+internal class InmemoryQueuePublisher : IPublishWeatherReports
+{
+    private readonly ConcurrentQueue<OnDemandWeatherReport> _queue;
+
+    public InmemoryQueuePublisher(ConcurrentQueue<OnDemandWeatherReport> queue)
+    {
+        _queue = queue;
+    }
+    public void Publish(OnDemandWeatherReport weatherReport)
+    {
+        _queue.Enqueue(weatherReport);
+    }
+}
