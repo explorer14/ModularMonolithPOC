c2ff85bd7f3dc3aa36d5c3b816fd8923a31c976d
explorer14 <amanag1983@gmail.com>
2024-07-30 13:51:15 +0200

    Publish the weather event triggered by a controller for now

---
+++ ModularMonolith.Api/WeatherReportsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc;
+using WeatherReporting.PublishedInterfaces;
+
+namespace ModularMonolith.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WeatherReportsController(IProvideOnDemandWeatherReport onDemandWeatherReportProvider,
+    ConcurrentQueue<OnDemandWeatherReport> queue)
+    : ControllerBase
+{
+    [HttpGet("{city}")]
+    public IActionResult GetWeatherReportForCity(string city)
+    {
+        var weatherReport = onDemandWeatherReportProvider.GetTodaysWeatherFor(city);
+        
+        return Ok(weatherReport);
+    }
+    
+    // This would usually be an internal process that publishes the event but to keep this
+    // example simple, we will trigger it manually
+    [HttpPost("publish/{city}")]
+    public IActionResult PublishNewWeatherReport(string city)
+    {
+        var weatherReport = onDemandWeatherReportProvider.GetTodaysWeatherFor(city);
+        queue.Enqueue(weatherReport);
+
+        return Accepted();
+    }
+}
