3e1bde4a2c08bd14407add3366cf9ef2855203d6
explorer14 <amanag1983@gmail.com>
2024-07-30 15:27:03 +0200

    Add a representative unit test after the fact to see if there is any testability concerns

--- WeatherReporting/WeatherReporting.DomainModel/AssemblyInfo.cs
+++ WeatherReporting/WeatherReporting.DomainModel/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 [assembly:InternalsVisibleTo("WeatherReporting.ExternalSources.Adapter")]
 [assembly:InternalsVisibleTo("WeatherReporting.Publishing.Adapter")]
 [assembly:InternalsVisibleTo("WeatherReporting.ApplicationServices")]
+[assembly:InternalsVisibleTo("WeatherReporting.UnitTests")]
---
+++ WeatherReporting/tests/WeatherReporting.UnitTests/WhenGettingTodaysWeatherReportForACity.cs
@@ -0,0 +1,32 @@
+using FluentAssertions;
+using WeatherReporting.ApplicationServices;
+using WeatherReporting.DomainModel;
+
+namespace WeatherReporting.UnitTests;
+
+public class WhenGettingTodaysWeatherReportForACity
+{
+    [Fact]
+    public void GivenCityShouldReturnWeatherReport()
+    {
+        var expectedWeatherReport = new PublishedInterfaces.OnDemandWeatherReport(TemperatureC: 10,
+            TemperatureF:50,
+            City:"any city",
+            ReportedOn: new DateTimeOffset(2024, 7, 30, 15, 16, 0, TimeSpan.FromHours(2)));
+        
+        var weatherReportProvider = new OnDemandWeatherReportProvider(
+            new StubWeatherRetriever(
+                new OnDemandWeatherReport(
+                    TemperatureC: 10,
+                    ReportedOn: new DateTimeOffset(2024, 7, 30, 15, 16, 0, TimeSpan.FromHours(2)))));
+        var actualWeatherReport = weatherReportProvider.GetTodaysWeatherFor("any city");
+        actualWeatherReport.Should().Be(expectedWeatherReport);
+    }
+}
+
+internal class StubWeatherRetriever(OnDemandWeatherReport weatherReportToReturn) 
+    : IRetrieveWeatherReport
+{
+    public OnDemandWeatherReport GetTodaysWeatherFor(string city) => 
+        weatherReportToReturn;
+}
