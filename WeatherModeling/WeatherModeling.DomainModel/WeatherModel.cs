12e69da99d6dc7d6a96e4141570560dabf1d394c
explorer14 <amanag1983@gmail.com>
2025-11-18 15:00:17 +0100

    Create a weather model object in the domain

---
+++ WeatherModeling/WeatherModeling.DomainModel/WeatherModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherModeling.DomainModel;
+
+public class WeatherModel(string name, string description, double[] parameters)
+{
+    public string Name { get; } = name;
+    public string Description { get; } = description;
+    public double[] Parameters { get; } = parameters;
+
+    public Guid Id { get; } = Guid.NewGuid();
+}
